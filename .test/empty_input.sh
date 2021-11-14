84fd036a8041945b469783a7699c0a55dbc60a85
Aleksandr Shakhov <alex1097@mail.ru>
2021-11-15 01:53:18 +0300

    Update empty_input.sh

--- .test/empty_input.sh
+++ .test/empty_input.sh
@@ -1,46 +1,21 @@
 #!/bin/bash
-
 set -e
 CC=clang
 CXX=clang++
+
 cp Makefile.soft Makefile
-make
-
-RES=$(echo '' | timeout 10s ./posix 10 10)
-
-if [[ $? != 0 ]]; then
-    git reset --hard HEAD
-    git clean -f
-    echo "FAIL"
-    echo 1
-fi
-
-
-if (( $RES == 0 )); then
-    git reset --hard HEAD
-    git clean -f
-    echo "OK"
-    exit 0
-else
-    git reset --hard HEAD
-    git clean -f
-    echo "FAIL must be 0 but return " $RES
-    exit 1
-fi
-
-set -e
-CC=clang
-CXX=clang++
-cp Makefile.soft ../Makefile
-cd ..
 echo "Step 1. Build"
 make
 
 echo "Step 2. Run test"
-echo "echo '' | timeout 10s ./posix 10 10"
-OUT=$(echo '' | timeout 10s ./posix 10 10)
+echo "echo '' > in.txt && timeout 10s ./posix 10 10"
 
+rm -f "in.txt"
+echo '' > "in.txt"
+OUT=$(timeout 10s ./posix 10 10)
 RC=$?
+
+rm -f "in.txt"
 
 git reset --hard HEAD
 git clean -f
